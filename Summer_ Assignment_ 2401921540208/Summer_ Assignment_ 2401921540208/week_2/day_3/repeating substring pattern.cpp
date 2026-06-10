7ce1c69272033a6e2d0af813dda554308b288cce
salilawasthi-fkd <salilawasthi308@gmail.com>
2026-06-10 22:31:52 +0530

    Add files via upload

---
+++ Summer_ Assignment_ 2401921540208/Summer_ Assignment_ 2401921540208/week_2/day_3/repeating substring pattern.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.size();
+
+        for (int len = 1; len <= n / 2; len++) {
+            if (n % len != 0) continue;
+
+            string pattern = s.substr(0, len);
+            string repeated = "";
+
+            int times = n / len;
+            for (int i = 0; i < times; i++) {
+                repeated += pattern;
+            }
+
+            if (repeated == s) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
