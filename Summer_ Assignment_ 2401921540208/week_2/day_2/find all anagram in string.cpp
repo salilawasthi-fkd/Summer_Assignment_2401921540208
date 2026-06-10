826dedb1f42080a972e732be719bd8f3f9d43a74
salilawasthi-fkd <salilawasthi308@gmail.com>
2026-06-10 22:33:51 +0530

    Add files via upload

---
+++ Summer_ Assignment_ 2401921540208/week_2/day_2/find all anagram in string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
+
+        int n = s.size(), m = p.size();
+        if (m > n) return ans;
+
+        vector<int> pCount(26, 0), winCount(26, 0);
+
+        for (int i = 0; i < m; i++) {
+            pCount[p[i] - 'a']++;
+            winCount[s[i] - 'a']++;
+        }
+
+        if (pCount == winCount)
+            ans.push_back(0);
+
+        for (int i = m; i < n; i++) {
+            winCount[s[i] - 'a']++;       // add new char
+            winCount[s[i - m] - 'a']--;   // remove old char
+
+            if (pCount == winCount)
+                ans.push_back(i - m + 1);
+        }
+
+        return ans;
+    }
+};
