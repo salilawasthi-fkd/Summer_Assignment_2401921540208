3799f5b4cbc1d7c6c0cd1810866fd39e05bfb069
salilawasthi-fkd <salilawasthi308@gmail.com>
2026-06-12 22:40:26 +0530

    Add files via upload

---
+++ Summer_ Assignment_ 2401921540208/week_2/day_5/group of anagram.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for (string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());  // Create canonical form
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> result;
+        for (auto& entry : mp) {
+            result.push_back(entry.second);
+        }
+
+        return result;
+    }
+};
